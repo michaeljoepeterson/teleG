f8cd6e14225bfca19d31acb5771334aebe1ed144
Michael Peterson <michaeljoepeterson@gmail.com>
2019-02-12 19:12:44 -0700

    get margins

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -1,14 +1,15 @@
 function GameBoard(gameBoardID){
 	this.gameBoard = document.getElementById(gameBoardID);
 	this.initBoardListener();
-	console.log(this.gameBoard);
+	//console.log(this.gameBoard);
 }
 
 GameBoard.prototype.initBoardListener = function(){
+	/*
 	this.gameBoard.addEventListener("mouseover",function(e){
 		this.mouseoverBoard(e);
 	}.bind(this),false);
-
+	*/
 	this.gameBoard.addEventListener("click",function(e){
 		this.clickBoard(e);
 	}.bind(this),false);
@@ -16,6 +17,8 @@ GameBoard.prototype.initBoardListener = function(){
 
 GameBoard.prototype.clickBoard = function(event){
 	console.log("test click");
+	//adjust poition with margins 20px top margin and other margins have to get
+	console.log("target margins ",getComputedStyle(event.currentTarget).getPropertyValue("margin-left"));
 	console.log(event.clientX,event.clientY);
 }
 
